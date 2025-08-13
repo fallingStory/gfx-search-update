d1e72e978136d8976f156a8b1eaeaa1103f1a00c
fallingStory <fallingstory1792@gmail.com>
2025-08-12 17:44:32 -0700

    added function to create dictionary from gfx file

--- gfx_search_update.py
+++ gfx_search_update.py
@@ -5,9 +5,13 @@ import subprocess
 def main():
     # FOR ONLY GOALS
     # Create an empty JSON file
+    json_from_gfx: dict = {}
+
     # Scan through all National Focus .gfx files and add entry for them in JSON, using filename as entry name and containing
         # gfx_name
         # filepath
+    fill_json_from_gfx(json_from_gfx, SOURCES)
+
     # Create directory "safe_images" in destination folder
     # Create a new empty JSON file NEW_J, to use filename as entry and contain
         # gfx_name (blank)
@@ -26,6 +30,34 @@ def main():
 
     pass
 
+def fill_json_from_gfx(json_from_gfx: dict, filename):
+    # Proccess all the entries in a gfx file
+    try:
+        with open(filename, 'r') as file:
+            open_bracket: bool = True
+            entry_filename: str = ""
+            entry_gfx_name: str = ""
+            entry_filepath: str = ""
+            for preprocessed_line in file:
+                # Figure out what to do with line in file
+                line = preprocessed_line.strip()
+                if line[-1] == "{":
+                    open_bracket = True
+                elif line[-1] == "}":
+                    if (open_bracket):
+                        open_bracket = False
+                        json_from_gfx[entry_filename] = {'gfx_name': entry_gfx_name, 'filepath': entry_filepath}
+                        entry_filename, entry_gfx_name, entry_filepath = "", "", ""
+                elif open_bracket:
+                    split_line = preprocessed_line.split()
+                    if split_line[0] == "name":
+                        entry_gfx_name = split_line[2].strip('"')
+                    elif split_line[0] == "texturefile":
+                        entry_filepath = split_line[2].strip('"')
+                        entry_filename = entry_filepath.split('/')[-1]
+
+    except FileNotFoundError:
+        print(f"Error: Cannot find \'{filename}\'.")
 
 def convert_to_png(image: str):
     image_png: str = image.split(".")[0] + ".png"
